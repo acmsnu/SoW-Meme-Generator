7d4997c99f0cf8ff166b37207cb5029f67f631fb
AadishJ <jain.aadishj@gmail.com>
2025-06-01 15:20:59 +0530

    Revert "Initial Commit"
    This reverts commit ec5f94f4d9259b485be0e5f122fb5a4c75f01409.

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const App = () => {
   return (
     <div className="flex items-center justify-center h-screen bg-gray-100 text-9xl">
-      API: https://api.imgflip.com/get_memes
+      App
     </div>
   )
 }
